38c3e910aa6f17eb07fc8441dc3f6d71b20cda07
Averor <averor.dev@gmail.com>
2018-04-12 14:27:30 +0200

    in progress

---
+++ src/SimpleBusPackage/Factory/CommandBusFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Averor\SimpleBusPackage\Factory;
+
+/**
+ * Class CommandBusFactory
+ *
+ * @package Averor\SimpleBusPackage\Factory
+ * @author JurekT <jerzy.tuszynski@sprint.pl>
+ */
+class CommandBusFactory
+{
+
+}
