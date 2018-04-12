93a3aca3afcbac61f6450b7ee03f05e11fdf9e58
Averor <averor.dev@gmail.com>
2018-04-12 14:57:56 +0200

    in progress

--- src/SimpleBusPackage/Factory/CommandBusFactory.php
+++ src/SimpleBusPackage/Factory/CommandBusFactory.php
@@ -6,11 +6,14 @@ namespace Averor\SimpleBusPackage\Factory;
 
 use Averor\SimpleBusPackage\Bus\CommandBus;
 use Averor\SimpleBusPackage\Resolver\InvokableHandlerResolver;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use SimpleBus\Message\Bus\MessageBus;
 use SimpleBus\Message\Bus\Middleware\FinishesHandlingMessageBeforeHandlingNext;
 use SimpleBus\Message\CallableResolver\CallableMap;
 use SimpleBus\Message\Handler\DelegatesToMessageHandlerMiddleware;
 use SimpleBus\Message\Handler\Resolver\NameBasedMessageHandlerResolver;
+use SimpleBus\Message\Logging\LoggingMiddleware;
 use SimpleBus\Message\Name\ClassBasedNameResolver;
 
 /**
@@ -25,11 +28,20 @@ class CommandBusFactory
      * @param array $map
      * @return MessageBus
      */
-    public static function create(array $map) : MessageBus
+    public static function create(array $map, ?LoggerInterface $logger = null) : MessageBus
     {
         $commandBus = new CommandBus([
             new FinishesHandlingMessageBeforeHandlingNext()
         ]);
+
+        if ($logger !== null) {
+            $commandBus->appendMiddleware(
+                new LoggingMiddleware(
+                    $logger,
+                    LogLevel::DEBUG
+                )
+            );
+        }
 
         $commandHandlerMap = new CallableMap(
             $map,
