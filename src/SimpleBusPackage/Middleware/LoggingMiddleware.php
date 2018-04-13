4e466a183456414799676b796fc7d5de0fae483c
Averor <averor.dev@gmail.com>
2018-04-13 12:08:04 +0200

    in progress

--- src/SimpleBusPackage/Middleware/LoggingMiddleware.php
+++ src/SimpleBusPackage/Middleware/LoggingMiddleware.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Averor\SimpleBusPackage\Middleware;
 
 use Averor\SimpleBusPackage\Message\Command;
+use Averor\SimpleBusPackage\Message\Event;
 use Averor\SimpleBusPackage\Message\Message;
+use Averor\SimpleBusPackage\Message\Query;
 use Psr\Log\LoggerInterface;
 use SimpleBus\Message\Bus\Middleware\MessageBusMiddleware;
 
@@ -34,48 +36,170 @@ class LoggingMiddleware implements MessageBusMiddleware
      * @return void
      * @throws \Throwable
      */
-    public function handle($message, callable $next)
+    public function handle($message, callable $next) : void
     {
-        if ($message instanceof Command) {
-            $this->logger->info(
-                sprintf(
-                    "Command %s [%s] dispatched",
-                    get_class($message),
-                    $message->uuid()
-                ),
-                [
-                    'message' => json_encode($message, JSON_FORCE_OBJECT)
-                ]
-            );
+        switch (true) {
+
+            case ($message instanceof Command):
+                $this->handleCommand($message, $next);
+                break;
+
+            case ($message instanceof Event):
+                $this->handleEvent($message, $next);
+                break;
+
+            case ($message instanceof Query):
+                $this->handleQuery($message, $next);
+                break;
+
+            default:
+                $this->logger->warning(
+                    sprintf(
+                        "Message %s [%s] of unknown type dispatched",
+                        get_class($message),
+                        $message->uuid()
+                    ),
+                    [
+                        'message' => json_encode($message, JSON_FORCE_OBJECT)
+                    ]
+                );
+                break;
         }
+    }
+
+    /**
+     * @param Command $message
+     * @param callable $next
+     * @return void
+     * @throws \Throwable
+     */
+    protected function handleCommand(Command $message, callable $next) : void
+    {
+        $this->logger->info(
+            sprintf(
+                "Command %s [%s] dispatched",
+                get_class($message),
+                $message->uuid()
+            ),
+            [
+                'message' => json_encode($message, JSON_FORCE_OBJECT)
+            ]
+        );
 
         try {
 
             $next($message);
 
-            if ($message instanceof Command) {
-                $this->logger->info(
-                    sprintf(
-                        "Command %s [%s] handled",
-                        get_class($message),
-                        $message->uuid()
-                    ),
-                    []
-                );
-            }
+            $this->logger->info(
+                sprintf(
+                    "Command %s [%s] handled",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                []
+            );
 
         } catch (\Throwable $e) {
 
-            if ($message instanceof Command) {
-                $this->logger->error(
-                    sprintf(
-                        "Exception during handling command %s [%s]",
-                        get_class($message),
-                        $message->uuid()
-                    ),
-                    ['exception' => $e]
-                );
-            }
+            $this->logger->error(
+                sprintf(
+                    "Exception during handling command %s [%s]",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                ['exception' => $e]
+            );
+
+            throw $e;
+        }
+    }
+
+    /**
+     * @param Event $message
+     * @param callable $next
+     * @return void
+     */
+    protected function handleEvent(Event $message, callable $next) : void
+    {
+        $this->logger->info(
+            sprintf(
+                "Event %s [%s] dispatched",
+                get_class($message),
+                $message->uuid()
+            ),
+            [
+                'message' => json_encode($message, JSON_FORCE_OBJECT)
+            ]
+        );
+
+        try {
+
+            $next($message);
+
+            $this->logger->info(
+                sprintf(
+                    "Event %s [%s] handled",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                []
+            );
+
+        } catch (\Throwable $e) {
+
+            $this->logger->error(
+                sprintf(
+                    "Exception during handling event %s [%s]",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                ['exception' => $e]
+            );
+        }
+    }
+
+    /**
+     * @param Query $message
+     * @param callable $next
+     * @return void
+     * @throws \Throwable
+     */
+    protected function handleQuery(Query $message, callable $next) : void
+    {
+        $this->logger->info(
+            sprintf(
+                "Query %s [%s] dispatched",
+                get_class($message),
+                $message->uuid()
+            ),
+            [
+                'message' => json_encode($message, JSON_FORCE_OBJECT)
+            ]
+        );
+
+        try {
+
+            $next($message);
+
+            $this->logger->info(
+                sprintf(
+                    "Query %s [%s] handled",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                []
+            );
+
+        } catch (\Throwable $e) {
+
+            $this->logger->error(
+                sprintf(
+                    "Exception during handling query %s [%s]",
+                    get_class($message),
+                    $message->uuid()
+                ),
+                ['exception' => $e]
+            );
 
             throw $e;
         }
